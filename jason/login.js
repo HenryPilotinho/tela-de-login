a41cbb2a38d6924a94d08331687fc7b3cfbfd1c8
henrypilotinho <henriquepilotl@hotmail.com>
2023-09-17 17:40:35 -0300

    att

--- jason/login.js
+++ jason/login.js
@@ -7,7 +7,7 @@ document.getElementById('open-register').addEventListener('click', moveOverlay)
 
 document.getElementById('open-login').addEventListener('click', moveOverlay)
 
-const formulario = document.querySelector("form")
+const formulario = document.querySelector("form ")
 const Inome = document.querySelector(".nome")
 const Iemail = document.querySelector(".email")
 const Isenha = document.querySelector(".senha")
